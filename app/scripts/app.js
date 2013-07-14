2904e9fc40f4202336d9e50891b594e58b7532ea
James Huston <jhuston@redventures.com>
2013-07-14 11:23:56 -0400

    initial working pages with service. delete and toggle status funcitonal.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,50 @@
 
-angular.module('bookshelf', [])
+angular.module('bookshelf', ['bookshelf.services'])
 
 .config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'BookshelfCtrl'
-    })
-    .when('/books', {
       templateUrl: 'views/books.html',
-      controller: 'BooksController'
+      controller: 'BookshelfController'
+    })
+    .when('/book', {
+      templateUrl: 'views/book.html',
+      controller: 'BookController'
+    })
+    .when('/book/:bookId', {
+      templateUrl: 'views/book.html',
+      controller: 'BookController'
     })
     .otherwise({
       redirectTo: '/'
     });
 })
 
-.controller('BookshelfController', function ($scope) {
+.controller('BookshelfController', function ($scope, BookService) {
+  $scope.bookArray = BookService.getBooks();
+
+  $scope.toggleStatus = function (bookId) {
+    var book = BookService.getBooks(bookId);
+
+    book.completed = !book.completed;
+
+    BookService.updateBook(bookId, book);
+  };
+
+  $scope.removeBook = function (bookId) {
+    BookService.deleteBook(bookId);
+  };
 
 })
 
-.controller('BooksController', function ($scope) {
-  
+.controller('BookController', function ($scope, BookService) {
+
+})
+
+.controller('NavigationController', function ($scope, $location) {
+  $scope.path = $location.path();
 })
 
 ;
+
+angular.module('bookshelf.services', []);
---
+++ app/scripts/services/bookdata.js
@@ -0,0 +1,73 @@
+
+angular.module('bookshelf.services')
+
+.service('BookService', function () {
+  var books = [
+    {
+      "title": "Functional JavaScript",
+      "author": "Michael Fogus",
+      "img": "functional-js.gif",
+      "completed": false
+    },
+    {
+      "title": "JavaScript: The Definitive Guide, 6th Edition",
+      "author": "David Flanagan",
+      "img": "js-definitive-guide.gif",
+      "completed": true
+    },
+    {
+      "title": "Node for Front-End Developers",
+      "author": "Garann Means",
+      "img": "node-for-fed.gif",
+      "completed": false
+    },
+    {
+      "title": "JavaScript Web Applications",
+      "author": "Alex MacCaw",
+      "img": "js-web-apps.gif",
+      "completed": false
+    },
+    {
+      "title": "Learning JavaScript Design Patterns",
+      "author": "Addy Osmani",
+      "img": "js-patterns.gif",
+      "completed": false
+    },
+    {
+      "title": "Maintainable JavaScript",
+      "author": "Nicholas C. Zakas",
+      "img": "maintainable-js.gif",
+      "completed": true
+    }
+  ];
+
+  return {
+    getBooks: function (bookId) {
+      if (bookId === undefined) {
+        return books;
+      }
+
+      bookId = parseInt(bookId);
+
+      return books[bookId];
+    },
+    setBooks: function (booksArray) {
+      books = booksArray
+    },
+    updateBook: function (bookId, bookData) {
+      books[bookId] = bookData;
+    },
+    addBook: function (bookData) {
+      books.push(bookData);
+    },
+    deleteBook: function (bookId) {
+      if (bookId === undefined) {
+        return;
+      }
+
+      bookId = parseInt(bookId);
+
+      books.splice(bookId, 1);
+    }
+  };
+});
